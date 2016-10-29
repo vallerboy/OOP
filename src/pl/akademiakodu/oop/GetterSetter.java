515ead0999931dc8afa55e2b1a21836612cea2c3
vallerboy <oskarbank@gmail.com>
2016-10-29 16:49:18 +0200

    asd

---
+++ src/pl/akademiakodu/oop/GetterSetter.java
@@ -0,0 +1,47 @@
+package pl.akademiakodu.oop;
+
+
+
+public class GetterSetter {
+ 
+ 
+	 public String title;
+	 public int pages; 
+	 public String author;
+	
+	public GetterSetter() { }
+
+	public GetterSetter(String title, String author, int pages) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getPages() {
+		return pages;
+	}
+
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	} 
+	 
+	 
+	
+	
+}
---
+++ src/pl/akademiakodu/oop/Main.java
@@ -0,0 +1,20 @@
+package pl.akademiakodu.oop;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		//  Stworzenie obiektu na pustym kostrzuktorze, z u¿yciem setterów.
+		GetterSetter obiekt = new GetterSetter();
+		obiekt.setAuthor("OskarP");
+		obiekt.setPages(24);
+		obiekt.setTitle("AkademiaKodu z pustego kons.");
+		
+		// Stworzenie kodu na innym kostruktorze (ale ta sama klasa), bez setterów
+		GetterSetter obiekt1 = new GetterSetter("AkademiaKodu.pl", "Oskar", 20);
+		 
+		System.out.println(obiekt.getTitle());
+		System.out.println(obiekt1.getTitle());
+	}
+
+}
